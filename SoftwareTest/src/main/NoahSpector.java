3705b00612706b3bcffda30c05082b61ff868bb9
noahspector <nlspector@gmail.com>
2018-05-23 21:17:03 -0400

    Because I want it to look nice

--- SoftwareTest/src/main/NoahSpector.java
+++ SoftwareTest/src/main/NoahSpector.java
@@ -11,7 +11,7 @@ public class NoahSpector extends SoftwareMember{
 		double randBound = 4 + Math.ceil(Math.random() * 12);
 		double randNum = Math.ceil(Math.random() * randBound);
 		if(randBound == 11) return "11 is the best number that has existed.";
-		return "Hi I am " + getName() + "and here is a random number between 1 and a random number between 5 and 16, 11 being the best: " + randNum;
+		return "Hi I am " + getName() + " and here is a random number between 1 and a random number between 5 and 16, 11 being the best: " + randNum;
 		
 	}
 	
