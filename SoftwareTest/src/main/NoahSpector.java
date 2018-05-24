df6381cd23c441dc8640318fe18e9dec5e08498b
noahspector <nlspector@gmail.com>
2018-05-23 21:27:58 -0400

    test123

--- SoftwareTest/src/main/NoahSpector.java
+++ SoftwareTest/src/main/NoahSpector.java
@@ -12,7 +12,7 @@ public class NoahSpector extends SoftwareMember{
 		double randNum = Math.ceil(Math.random() * randBound);
 		if(randBound == 11) return "11 is the best number that has existed.";
 		return "Hi I am " + getName() + " and here is a random number between 1 and a random number between 5 and 16, 11 being the best: " + randNum;
-		
+		//This is a comment added by not nlspector
 	}
 	
 }
